0218d6ff453af46719c1fc97141e2b2c3bbe8593
Kokorononamae12 <ade554982@gmail.com>
2026-08-17 00:24:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("Welcome to Mirai's World 💞");
+
+const button =
+document.getElementById("miraibutton");
+
+const message = 
+document.getElementById("message");
+
+button.addEventListener("click",function() {
+    message.textContent = "Mirai is watching you... 💞"
+});
